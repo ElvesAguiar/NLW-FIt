c03415487cc7bba50513c64ea6f39485b3d61ea2
Elves Aguiar <elvesmhs@hotmail.com>
2022-05-07 19:45:25 -0300

    day2 menu toggle

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function onScroll (){
-   // alert("oi")
-}
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () =>{
+    navUL.classList.toggle('show');
+})
